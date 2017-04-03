dd1f6423ad17a69100f3f59cca6ee7aa76bfb825
Yeonwoo Cho <eow0712@skku.edu>
2017-04-03 15:32:59 +0000

    bamboo

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+    @posts=Post.all
+  end
+
+  def new
+    @post=Post.new
+    @posts=Post.all
+  end
+  
+  def create
+    post = Post.new(post_params)
+    post.save
+    #redirect_to :main
+    redirect_to :back
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
