2aeb4e9c296df5a2b72bbfc81b20d45073c635d0
Yeonwoo Cho <eow0712@skku.edu>
2017-05-23 09:13:26 +0000

    search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'posts/new' => 'posts#new', as: 'new_post'
 
   root 'posts#index'
+  
+  resources 'posts'
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
